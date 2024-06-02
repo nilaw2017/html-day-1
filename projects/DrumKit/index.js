f58505f71841446e3093ea3fb46f4a8029e9218e
nilaw2017 <nilaw.manandhar2016@gmail.com>
2024-06-02 13:56:51 +0545

    Practice

---
+++ projects/DrumKit/index.js
@@ -0,0 +1,76 @@
+// document.querySelector("button").addEventListener("click", function () {
+//   console.log("Dhinchakk");
+// });
+
+// document.querySelectorAll(".chandan")[1].addEventListener("click", function () {
+//   console.log("Dhinchak");
+//   let w = new Audio("./sounds/crash.mp3");
+//   w.play();
+// });
+
+// document.querySelectorAll(".deepesh")[1].addEventListener("click", function () {
+//   console.log("Tak tak");
+//   let w = new Audio("./sounds/kick-bass.mp3");
+//   w.play();
+// });
+
+document.querySelector(".drumstick").addEventListener("click", function () {
+  console.log("Pooja");
+  let w = new Audio("./sounds/snare.mp3");
+  w.play();
+});
+
+$(".drumstick").click(function () {
+  console.log("Pooja");
+  let w = new Audio("./sounds/snare.mp3");
+  w.play();
+});
+
+// document.querySelectorAll("button")[3].addEventListener("click", function () {
+//   console.log("Chanda");
+//   let w = new Audio("./sounds/tom-1.mp3");
+//   w.play();
+// });
+
+// document.querySelectorAll("button")[4].addEventListener("click", function () {
+//   console.log("Deepa");
+//   let w = new Audio("./sounds/tom-2.mp3");
+//   w.play();
+// });
+
+// document.querySelectorAll("button")[5].addEventListener("click", function () {
+//   console.log("Neelu");
+//   let w = new Audio("./sounds/tom-3.mp3");
+//   w.play();
+// });
+
+// document.querySelectorAll("button")[6].addEventListener("click", function () {
+//   console.log("Kissu");
+//   let w = new Audio("./sounds/tom-4.mp3");
+//   w.play();
+// });
+
+// console.log(document.querySelector("button"));
+// console.log(document.querySelectorAll("button")[1]);
+
+// function add(a, b) {
+//   var sum = a + b;
+
+//   console.log(sum);
+// }
+// var deepesh = "handsome";
+// const krishna = "turu";
+// function clothes() {
+//   let chandan = "white";
+// }
+
+// function sub(a, b) {
+//   var sub = a - b;
+//   console.log(sub);
+// }
+// function operator(a, b, operation) {
+//   operation(a, b);
+// }
+
+// operator(2, 5, sub);
+// operator(5, 5, add);
---
+++ public/js/function.js
@@ -0,0 +1,3 @@
+var b = 56;
+console.log(`Deepesh's mother says,"He should practice ${b} times"`);
+console.log("Deepesh's practice should be", b, "times");
