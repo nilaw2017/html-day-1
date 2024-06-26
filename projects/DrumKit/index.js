453dc2fd4ec5a2fda2fbac1abf3f1f8367338995
nilaw2017 <nilaw.manandhar2016@gmail.com>
2024-06-26 13:05:27 +0545

    [FE]:: operators arithmetic, assignment, comparative

--- projects/DrumKit/index.js
+++ projects/DrumKit/index.js
@@ -1,76 +1,81 @@
 // document.querySelector("button").addEventListener("click", function () {
 //   console.log("Dhinchakk");
 // });
-
-// document.querySelectorAll(".chandan")[1].addEventListener("click", function () {
-//   console.log("Dhinchak");
-//   let w = new Audio("./sounds/crash.mp3");
-//   w.play();
-// });
-
-// document.querySelectorAll(".deepesh")[1].addEventListener("click", function () {
-//   console.log("Tak tak");
-//   let w = new Audio("./sounds/kick-bass.mp3");
-//   w.play();
-// });
-
-document.querySelector(".drumstick").addEventListener("click", function () {
-  console.log("Pooja");
-  let w = new Audio("./sounds/snare.mp3");
+console.log("TEST");
+document.querySelectorAll("button")[0].addEventListener("click", function () {
+  console.log("Dhinchak");
+  let w = new Audio("./sounds/crash.mp3");
   w.play();
 });
 
-$(".drumstick").click(function () {
-  console.log("Pooja");
-  let w = new Audio("./sounds/snare.mp3");
+document.querySelectorAll("button")[1].addEventListener("click", function () {
+  console.log("Tak tak");
+  let w = new Audio("./sounds/kick-bass.mp3");
   w.play();
 });
 
-// document.querySelectorAll("button")[3].addEventListener("click", function () {
-//   console.log("Chanda");
-//   let w = new Audio("./sounds/tom-1.mp3");
+// document.querySelector(".drumstick").addEventListener("click", function () {
+//   console.log("Pooja");
+//   let w = new Audio("./sounds/snare.mp3");
 //   w.play();
 // });
 
-// document.querySelectorAll("button")[4].addEventListener("click", function () {
-//   console.log("Deepa");
-//   let w = new Audio("./sounds/tom-2.mp3");
+// $(".drumstick").click(function () {
+//   console.log("Pooja");
+//   let w = new Audio("./sounds/snare.mp3");
 //   w.play();
 // });
 
-// document.querySelectorAll("button")[5].addEventListener("click", function () {
-//   console.log("Neelu");
-//   let w = new Audio("./sounds/tom-3.mp3");
-//   w.play();
-// });
+document.querySelectorAll("button")[2].addEventListener("click", function () {
+  console.log("Chanda");
+  let w = new Audio("./sounds/snare.mp3");
+  w.play();
+});
+document.querySelectorAll("button")[3].addEventListener("click", function () {
+  console.log("Chanda");
+  let w = new Audio("./sounds/tom-1.mp3");
+  w.play();
+});
 
-// document.querySelectorAll("button")[6].addEventListener("click", function () {
-//   console.log("Kissu");
-//   let w = new Audio("./sounds/tom-4.mp3");
-//   w.play();
-// });
+document.querySelectorAll("button")[4].addEventListener("click", function () {
+  console.log("Deepa");
+  let w = new Audio("./sounds/tom-2.mp3");
+  w.play();
+});
 
-// console.log(document.querySelector("button"));
-// console.log(document.querySelectorAll("button")[1]);
+document.querySelectorAll("button")[5].addEventListener("click", function () {
+  console.log("Neelu");
+  let w = new Audio("./sounds/tom-3.mp3");
+  w.play();
+});
 
-// function add(a, b) {
-//   var sum = a + b;
+document.querySelectorAll("button")[6].addEventListener("click", function () {
+  console.log("Kissu");
+  let w = new Audio("./sounds/tom-4.mp3");
+  w.play();
+});
 
-//   console.log(sum);
-// }
-// var deepesh = "handsome";
-// const krishna = "turu";
-// function clothes() {
-//   let chandan = "white";
-// }
+console.log(document.querySelector("button"));
+console.log(document.querySelectorAll("button")[1]);
 
-// function sub(a, b) {
-//   var sub = a - b;
-//   console.log(sub);
-// }
-// function operator(a, b, operation) {
-//   operation(a, b);
-// }
+function add(a, b) {
+  var sum = a + b;
 
-// operator(2, 5, sub);
-// operator(5, 5, add);
+  console.log(sum);
+}
+var deepesh = "handsome";
+const krishna = "turu";
+function clothes() {
+  let chandan = "white";
+}
+
+function sub(a, b) {
+  var sub = a - b;
+  console.log(sub);
+}
+function operator(a, b, operation) {
+  operation(a, b);
+}
+
+operator(2, 5, sub);
+operator(5, 5, add);
---
+++ public/js/logical.js
@@ -0,0 +1,12 @@
+var a = 0;
+var b = 0;
+var c = 10;
+var d = 10;
+
+// if (a === b || c !== d) {
+//   console.log("TRUE");
+// } else {
+//   console.log("FALSE");
+// }
+
+!(a === b || c !== d) ? console.log("TRUE") : console.log("FALSE");
